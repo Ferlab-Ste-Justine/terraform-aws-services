7df99e78fe29e193e0bc5ed478551ab91dd48935
Eric Vallee <eric_vallee@webificservices.com>
2026-08-21 17:59:01 -0400

    feat: INFRA-1625 Added starrocks and heartbeat configuration

--- amp-configuration/conf.tf
+++ amp-configuration/conf.tf
@@ -1,9 +1,11 @@
 module "prometheus_confs" {
-  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-prometheus-configuration.git?ref=v0.3.0"
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-prometheus-configuration.git?ref=v0.3.1"
   terracd_jobs             = var.terracd_jobs
   node_exporter_jobs       = var.node_exporter_jobs
   blackbox_exporter_jobs   = var.blackbox_exporter_jobs
   kubernetes_exporter_jobs = var.kubernetes_exporter_jobs
+  starrocks_exporter_jobs  = var.starrocks_exporter_jobs
+  heartbeat                = var.heartbeat
 }
 
 resource "aws_prometheus_rule_group_namespace" "prometheus_confs" {
--- amp-configuration/variables.tf
+++ amp-configuration/variables.tf
@@ -66,6 +66,50 @@ variable "kubernetes_exporter_jobs" {
   default = []
 }
 
+variable "starrocks_exporter_jobs" {
+  description = "List of starrocks exporter jobs"
+  type = list(object({
+    tag                       = string
+    fe = object({
+      count                       = number
+      heap_usage_threshold        = optional(number)
+      query_error_rate_threshold  = number
+      compaction_score_threshold  = optional(number, 100)
+      meta_log_count_threshold    = optional(number, 100000)
+      p95_query_latency_threshold = optional(number)
+      txn_publish_delay_threshold = optional(number)
+    })
+    be = object({
+      count                      = number
+      cpu_usage_threshold        = optional(number)
+      cpus_per_node              = optional(number)
+      memory_usage_threshold     = optional(number)
+      memory_per_node            = optional(number)
+      disk_space_usage_threshold = optional(number)
+      disk_io_usage_threshold    = optional(number)
+    })
+    alert_labels              = map(string)
+  }))
+  default = []
+}
+
+
+variable "heartbeat" {
+  description = "Parameters for a heartbeat alert that triggers approximately around a given utc time each day and confirms alerting is still operational"
+  type = object({
+    enabled      = optional(bool, true)
+    hour         = number
+    minute       = number
+    alert_labels = map(string)
+  })
+  default = {
+    enabled      = false
+    hour         = 0
+    minute       = 0
+    alert_labels = {}
+  }
+}
+
 variable "workspace_id" {
   description = "Id of the prometheus workspace to attach the configuration to"
   type = string
